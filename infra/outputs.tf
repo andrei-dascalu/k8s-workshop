3a65bfde1e61c8db94758d497a42ea7d9d47ea63
Andrei Dascalu <andrei.dascalu@qubiz.com>
2021-08-13 12:58:01 +0300

    state update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
-output "k8s_ingress_ip" {
+output "k8s_ingress_ipvalue" {
   description = "IP for ingress"
   value       = google_compute_global_address.ip_address.address
 }
 
-output "k8s_ingress_name" {
+output "k8s_ingress_ipname" {
   description = "Name for ingress"
   value       = google_compute_global_address.ip_address.name
 }
